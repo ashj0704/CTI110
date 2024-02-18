583c140c8c85c03dfdb7d31721331617654cf0cc
ashj0704 <160434808+ashj0704@users.noreply.github.com>
2024-02-18 17:24:43 -0500

    P1LAB1_AshJordan

---
+++ P1LAB1_AshJordan.py
@@ -0,0 +1,10 @@
+print('# Jordan Ash')
+
+print('# 2/18/2024')
+
+print('# P1LAB1')
+
+print('# This assignment is an introduction to using Python and learning its general functions, input/output, programming, and much more.')
+
+print('Jordan')
+print('Welcome to Python')

e49725dced85c45f9af4d64089fe6d71f209b4b7
ashj0704 <160434808+ashj0704@users.noreply.github.com>
2024-02-25 22:22:28 -0500

    Add files via upload

---
+++ P1HW2_AshJordan.py
@@ -0,0 +1,29 @@
+# Jordan Ash
+
+# 2/25/2024
+
+# P1HW2
+
+# Program for basic math for travel.
+
+
+print('Enter Budget:' ,1500)
+
+print('Enter your travel destination:' ,"St Louis MO")
+
+print('How much will you spend on gas?' ,200)
+
+print('How much will you spend on accomodations?' ,800)
+
+print('How much will you spend on food?' ,200)
+
+print('------------Total Expenses------------')
+
+print('Location: St Louis MO')
+print('Budget: 1500')
+
+print('Gas: 200')
+print('Accomodation: 800')
+print('Food: 200')
+
+print('Remaining Balance: 300')

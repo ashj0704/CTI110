4ff9e0e3649928fb5d18db0f0b2bb4c0a450bff0
ashj0704 <160434808+ashj0704@users.noreply.github.com>
2024-03-03 21:57:02 -0500

    Add files via upload

---
+++ P2LAB2_AshJordan.py
@@ -0,0 +1,21 @@
+# Jordan Ash
+
+# 3/3/2024
+
+# P2LAB2
+
+# Simple Statistics
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+num4 = float(input("Enter the fourth number: "))
+
+product = num1 * num2 * num3 * num4
+average = (num1 + num2 + num3 + num4) / 4
+
+print(f"Product as integer: {product:.0f}")
+print(f"Average as integer: {average:.0f}")
+print(f"Product as float: {product:.3f}")
+print(f"Average as float: {average:.3f}")
